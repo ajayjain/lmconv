bfe0eaf7d2937a07ba72638f9a49d1365e037f0f
Ajay Jain <ajayj@berkeley.edu>
2019-12-20 18:15:47 -0800

    Add --mode flag to support sampling

--- main.py
+++ main.py
@@ -28,7 +28,9 @@ parser.add_argument('-d', '--dataset', type=str,
 parser.add_argument('-p', '--print_every', type=int, default=50,
                     help='how many iterations between print statements')
 parser.add_argument('-t', '--save_interval', type=int, default=10,
-                    help='Every how many epochs to write checkpoint/samples?')
+                    help='Every how many epochs to write checkpoint?')
+parser.add_argument('-ts', '--sample_interval', type=int, default=2,
+                    help='Every how many epochs to write samples?')
 parser.add_argument('-r', '--load_params', type=str, default=None,
                     help='Restore training from previous model checkpoint?')
 parser.add_argument('--exp_name', type=str, default=None)
@@ -64,6 +66,8 @@ parser.add_argument('-af', '--accum_freq', type=int, default=1,
 parser.add_argument('--two_stream', action="store_true", help="Enable two stream model")
 parser.add_argument('--order', type=str, choices=["raster_scan", "s_curve", "hilbert", "gilbert2d"],
                     help="Autoregressive generation order")
+parser.add_argument('--mode', type=str, choices=["train", "sample"],
+                    default="train")
 
 args = parser.parse_args()
 
@@ -75,7 +79,6 @@ model_name = 'pcnn_{}_lr{:.5f}_wd{}_gc{}_nr-resnet{}_nr-filters{}_k{}_md{}_bs{}'
 if args.exp_name:
     model_name = f'{model_name}_{args.exp_name}'
 assert not os.path.exists(os.path.join('runs', model_name)), '{} already exists!'.format(model_name)
-writer = SummaryWriter(log_dir=os.path.join('runs', model_name))
 
 print("Model name:", model_name)
 print("Args:")
@@ -140,7 +143,8 @@ if args.ours:
 
     print(f"Mask shapes: {mask_init.shape}, {mask_undilated.shape}, {mask_dilated.shape}")
 else:
-    assert False
+    assert False  # DEBUG
+    generation_idx = get_generation_order_idx("raster_scan", obs[1], obs[2])
     model = PixelCNN(nr_resnet=args.nr_resnet, nr_filters=args.nr_filters, 
                 input_channels=input_channels, nr_logistic_mix=args.nr_logistic_mix)
     mask_init, mask_undilated, mask_dilated = None, None, None
@@ -148,140 +152,179 @@ model = nn.DataParallel(model)
 model = model.cuda()
 
 if args.load_params:
-    load_part_of_model(model, args.load_params)
-    # model.load_state_dict(torch.load(args.load_params))
-    print('model parameters loaded')
+    # TODO: Restore optimizer
+    checkpoint_epochs = load_part_of_model(args.load_params, model=model.module, optimizer=None)
+    print(f'model parameters loaded, from after {checkpoint_epochs} training epochs')
+else:
+    checkpoint_epochs = -1
 
 # NOTE: PixelCNN++ TF repo uses betas=(0.95, 0.9995), different than PyTorch defaults
 optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 scheduler = lr_scheduler.StepLR(optimizer, step_size=1, gamma=args.lr_decay)
 
-def sample(model):
+def sample(model, generation_idx, verbose=False):
     model.eval()
     data = torch.zeros(sample_batch_size, obs[0], obs[1], obs[2])
     data = data.cuda()
-    for i in range(obs[1]):
-        for j in range(obs[2]):
-            data_v = Variable(data)
-            out = model(data_v, sample=True, mask_init=mask_init, mask_undilated=mask_undilated, mask_dilated=mask_dilated)
-            out_sample = sample_op(out)
-            data[:, :, i, j] = out_sample.data[:, :, i, j]
+    for num_px_sampled, (i, j) in enumerate(generation_idx):
+    # for i in range(obs[1]):
+    #     for j in range(obs[2]):
+        data_v = Variable(data)
+        out = model(data_v, sample=True, mask_init=mask_init, mask_undilated=mask_undilated, mask_dilated=mask_dilated)
+        out_sample = sample_op(out)
+        data[:, :, i, j] = out_sample.data[:, :, i, j]
+
+        if num_px_sampled % 10 == 0 and verbose:
+            print(f"Sampled {num_px_sampled} pixels")
     return data
 
-print('starting training')
-global_step = 0
-min_train_bpd = 1e12
-min_test_bpd = 1e12
-for epoch in range(args.max_epochs):
-    train_loss = 0.
-    time_ = time.time()
-    model.train()
-    for batch_idx, (input,_) in enumerate(tqdm(train_loader, desc=f"Train epoch {epoch}")):
-        input = input.cuda(non_blocking=True)
-
-        if not torch.isfinite(input).all().cpu().item():
-            print("ERROR: main.py: NaN or Inf in input, embedding")
-            embed()
-
-        output = model(input, mask_init=mask_init, mask_undilated=mask_undilated, mask_dilated=mask_dilated)
-
-        if not torch.isfinite(output).all().cpu().item():
-            print("ERROR: main.py: NaN or Inf in returned tensor, embedding")
-            embed()
-
-        loss = loss_op(input, output)
-
-        if not torch.isfinite(loss).all().cpu().item():
-            print("ERROR: main.py: NaN or Inf in loss, embedding")
-            embed()
-
-        if batch_idx % args.accum_freq == 0:
-            optimizer.zero_grad()
-        loss.backward()
-        if (batch_idx + 1) % args.accum_freq == 0:
-            if args.clip > 0:
-                # Compute and rescale gradient norm
-                gradient_norm = nn.utils.clip_grad_norm_(model.parameters(), args.clip)
-                if gradient_norm > args.clip:
-                    print("WARN: Clipped gradients")
-                else:
-                    print("No need to clip gradients")
-            else:
-                # Just compute the gradient norm
-                parameters = list(filter(lambda p: p.grad is not None, model.parameters()))
-                gradient_norm = 0
-                for p in parameters:
-                    param_norm = p.grad.data.norm(2)
-                    gradient_norm += param_norm.item() ** 2
-                gradient_norm = gradient_norm ** (1. / 2)
-            writer.add_scalar('train/gradient_norm', gradient_norm, global_step)
-            optimizer.step()
-        train_loss += loss.item()
-
-        deno = args.batch_size * np.prod(obs) * np.log(2.)
-        assert deno > 0, embed()
-        train_bpd = loss.item() / deno
-        writer.add_scalar('train/bpd', train_bpd, global_step)
-        min_train_bpd = min(min_train_bpd, train_bpd)
-        writer.add_scalar('train/min_bpd', min_train_bpd, global_step)
-
-        if batch_idx >= 100 and (loss.item() / deno) >= 10:
-            print("WARNING: main.py: large batch loss {} bpd".format(loss.item() / deno))
-            # pdb.set_trace()
-
-        if (batch_idx + 1) % args.print_every == 0: 
-            deno = args.print_every * args.batch_size * np.prod(obs) * np.log(2.)
-            print('train bpd : {:.4f}, train loss : {:.1f}, time : {:.4f}, global step: {}'.format(
-                (train_loss / deno),
-                train_loss,
-                (time.time() - time_),
-                global_step))
-            train_loss = 0.
-            time_ = time.time()
-
-        if (batch_idx + 1) % args.accum_freq == 0:
-            global_step += 1
-
-    # decrease learning rate
-    scheduler.step()
-
-    model.eval()
-    with torch.no_grad():
-        test_loss = 0.
-        for batch_idx, (input,_) in enumerate(tqdm(test_loader, desc=f"Test epoch {epoch}")):
+if args.mode == "train":
+    print('starting training')
+    writer = SummaryWriter(log_dir=os.path.join('runs', model_name))
+    global_step = 0
+    min_train_bpd = 1e12
+    min_test_bpd = 1e12
+    for epoch in range(checkpoint_epochs + 1, args.max_epochs):
+        train_loss = 0.
+        time_ = time.time()
+        model.train()
+        for batch_idx, (input,_) in enumerate(tqdm(train_loader, desc=f"Train epoch {epoch}")):
             input = input.cuda(non_blocking=True)
-            input_var = Variable(input)
-            output = model(input_var, mask_init=mask_init, mask_undilated=mask_undilated, mask_dilated=mask_dilated)
-            loss = loss_op(input_var, output)
-    
-            if not torch.isfinite(loss).all().cpu().item():
-                print("ERROR: main.py: NaN or Inf in test loss in test loop, embedding")
+
+            if not torch.isfinite(input).all().cpu().item():
+                print("ERROR: main.py: NaN or Inf in input, embedding")
                 embed()
 
-            test_loss += loss.item()
-            del loss, output
+            output = model(input, mask_init=mask_init, mask_undilated=mask_undilated, mask_dilated=mask_dilated)
 
-        deno = batch_idx * args.batch_size * np.prod(obs) * np.log(2.)
-        assert deno > 0, embed()
-        test_bpd = test_loss / deno
-        writer.add_scalar('test/bpd', test_bpd, global_step)
-        min_test_bpd = min(min_test_bpd, test_bpd)
-        writer.add_scalar('test/min_bpd', min_test_bpd, global_step)
-        print('test loss : %s' % test_bpd)
+            if not torch.isfinite(output).all().cpu().item():
+                print("ERROR: main.py: NaN or Inf in returned tensor, embedding")
+                embed()
 
-        if (epoch + 1) % args.save_interval == 0: 
-            torch.save({
-                "epoch": epoch,
-                "test_loss": test_bpd,
-                "model_state_dict": model.module.state_dict(),
-                "optimizer_state_dict": optimizer.state_dict(),
-                "args": vars(args),
-                "mask_init": mask_init,
-                "mask": mask
-            }, 'models/{}_{}.pth'.format(model_name, epoch))
+            loss = loss_op(input, output)
 
-            print('sampling...')
-            sample_t = sample(model)
-            sample_t = rescaling_inv(sample_t)
-            utils.save_image(sample_t,'images/{}_{}.png'.format(model_name, epoch), 
-                    nrow=5, padding=0)
+            if not torch.isfinite(loss).all().cpu().item():
+                print("ERROR: main.py: NaN or Inf in loss, embedding")
+                embed()
+
+            if batch_idx % args.accum_freq == 0:
+                optimizer.zero_grad()
+            loss.backward()
+            if (batch_idx + 1) % args.accum_freq == 0:
+                if args.clip > 0:
+                    # Compute and rescale gradient norm
+                    gradient_norm = nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+                    if gradient_norm > args.clip:
+                        print("WARN: Clipped gradients")
+                    else:
+                        print("No need to clip gradients")
+                else:
+                    # Just compute the gradient norm
+                    parameters = list(filter(lambda p: p.grad is not None, model.parameters()))
+                    gradient_norm = 0
+                    for p in parameters:
+                        param_norm = p.grad.data.norm(2)
+                        gradient_norm += param_norm.item() ** 2
+                    gradient_norm = gradient_norm ** (1. / 2)
+                writer.add_scalar('train/gradient_norm', gradient_norm, global_step)
+                optimizer.step()
+            train_loss += loss.item()
+
+            deno = args.batch_size * np.prod(obs) * np.log(2.)
+            assert deno > 0, embed()
+            train_bpd = loss.item() / deno
+            writer.add_scalar('train/bpd', train_bpd, global_step)
+            min_train_bpd = min(min_train_bpd, train_bpd)
+            writer.add_scalar('train/min_bpd', min_train_bpd, global_step)
+
+            if batch_idx >= 100 and (loss.item() / deno) >= 10:
+                print("WARNING: main.py: large batch loss {} bpd".format(loss.item() / deno))
+                # pdb.set_trace()
+
+            if (batch_idx + 1) % args.print_every == 0: 
+                deno = args.print_every * args.batch_size * np.prod(obs) * np.log(2.)
+                print('train bpd : {:.4f}, train loss : {:.1f}, time : {:.4f}, global step: {}'.format(
+                    (train_loss / deno),
+                    train_loss,
+                    (time.time() - time_),
+                    global_step))
+                train_loss = 0.
+                time_ = time.time()
+
+            if (batch_idx + 1) % args.accum_freq == 0:
+                global_step += 1
+
+        # decrease learning rate
+        scheduler.step()
+
+        model.eval()
+        with torch.no_grad():
+            test_loss = 0.
+            for batch_idx, (input,_) in enumerate(tqdm(test_loader, desc=f"Test epoch {epoch}")):
+                input = input.cuda(non_blocking=True)
+                input_var = Variable(input)
+                output = model(input_var, mask_init=mask_init, mask_undilated=mask_undilated, mask_dilated=mask_dilated)
+                loss = loss_op(input_var, output)
+        
+                if not torch.isfinite(loss).all().cpu().item():
+                    print("ERROR: main.py: NaN or Inf in test loss in test loop, embedding")
+                    embed()
+
+                test_loss += loss.item()
+                del loss, output
+
+            deno = batch_idx * args.batch_size * np.prod(obs) * np.log(2.)
+            assert deno > 0, embed()
+            test_bpd = test_loss / deno
+            writer.add_scalar('test/bpd', test_bpd, global_step)
+            min_test_bpd = min(min_test_bpd, test_bpd)
+            writer.add_scalar('test/min_bpd', min_test_bpd, global_step)
+            print('test loss : %s' % test_bpd)
+
+            if (epoch + 1) % args.save_interval == 0: 
+                print('saving model...')
+                torch.save({
+                    "epoch": epoch,
+                    "test_loss": test_bpd,
+                    "model_state_dict": model.module.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "args": vars(args),
+                    # "mask_init": mask_init,
+                    # "mask": mask
+                }, 'models/{}_{}.pth'.format(model_name, epoch))
+
+
+            if (epoch + 1) % args.sample_interval == 0: 
+                print('sampling images...')
+                sample_t = sample(model, generation_idx)
+                sample_t = rescaling_inv(sample_t)
+                utils.save_image(sample_t,'images/{}_{}.png'.format(model_name, epoch), 
+                        nrow=5, padding=0)
+elif args.mode == "sample":
+    model.eval()
+    with torch.no_grad():
+        # test_loss = 0.
+        # for batch_idx, (input,_) in enumerate(tqdm(test_loader, desc=f"Test epoch {epoch}")):
+        #     input = input.cuda(non_blocking=True)
+        #     input_var = Variable(input)
+        #     output = model(input_var, mask_init=mask_init, mask_undilated=mask_undilated, mask_dilated=mask_dilated)
+        #     loss = loss_op(input_var, output)
+    
+        #     if not torch.isfinite(loss).all().cpu().item():
+        #         print("ERROR: main.py: NaN or Inf in test loss in test loop, embedding")
+        #         embed()
+
+        #     test_loss += loss.item()
+        #     del loss, output
+
+        # deno = batch_idx * args.batch_size * np.prod(obs) * np.log(2.)
+        # assert deno > 0, embed()
+        # test_bpd = test_loss / deno
+        # min_test_bpd = min(min_test_bpd, test_bpd)
+        # print('test loss : %s' % test_bpd)
+
+        print('sampling images...')
+        sample_t = sample(model, generation_idx, verbose=True)
+        sample_t = rescaling_inv(sample_t)
+        utils.save_image(sample_t,'images/{}_{}.png'.format(model_name, checkpoint_epochs), 
+                nrow=5, padding=0)
