add854e689102443b150ea1f1f4374f2443388b2
Ajay Jain <ajayj@berkeley.edu>
2019-12-21 12:12:52 -0800

    Remove unused raster scan mask method, add order plotting fns

--- masking.py
+++ masking.py
@@ -1,4 +1,5 @@
 # Masking utilities
+import math
 
 from hilbertcurve.hilbertcurve import HilbertCurve
 import matplotlib
@@ -10,41 +11,9 @@ import torch
 from gilbert2d import gilbert2d_idx
 
 
-def get_input_mask_raster_scan(kernel_size, num_patches, mask_type='B'):
-    """Generate mask for unfolded input image according to raster scan (row-major)
-    autoregressive generation ordering.
-
-    Args:
-        kernel_size (int or Tuple[int, int]): kernel size of corresponding convolution.
-                                              Influences rows in mask (ie patch size)
-        num_patches (int): width of unfolded input, corresponding to the number of
-                           locations a convolution will be applied. Generally equal
-                           to H * W if appropriate padding is applied to the input
-                           image before unfolding.
-        mask_type (str): A or B, determining whether the center pixel can be conditioned
-                         upon.
-
-    Returns: Mask of shape 1 x (kernel_size^2) x num_patches,
-             type torch.tensor(dtype=float)
-    """
-    if hasattr(kernel_size, "__iter__"):
-        assert kernel_size[0] == kernel_size[1]
-        kernel_size = kernel_size[0]
-
-    assert kernel_size == 3, "Only kernel size 3 supported"
-    assert mask_type in ["A", "B"]
-    
-    if mask_type == "A":
-        col_mask_chunk = torch.tensor([1, 1, 1,
-                                       1, 0, 0,
-                                       0, 0, 0], dtype=torch.uint8)
-    else:
-        col_mask_chunk = torch.tensor([1, 1, 1,
-                                       1, 1, 0,
-                                       0, 0, 0], dtype=torch.uint8)
-
-    inp_mask = col_mask_chunk.unsqueeze(1).repeat(1, num_patches)
-    return inp_mask.unsqueeze(0)
+####################
+# Generation orders
+####################
 
 def raster_scan_idx(rows, cols):
     """Return indices of a raster scan"""
@@ -79,6 +48,77 @@ def get_generation_order_idx(order: str, rows: int, cols: int):
     """Get (rows*cols) x 2 np array given order that pixels are generated"""
     assert order in ["raster_scan", "s_curve", "hilbert", "gilbert2d"]
     return eval(f"{order}_idx")(rows, cols)
+
+def reflect_rows(generation_idx, obs):
+    return list(map(lambda loc: [obs[1] - loc[0] - 1, loc[1]], generation_idx))
+
+def reflect_cols(generation_idx, obs):
+    return list(map(lambda loc: [loc[0], obs[2] - loc[1] - 1], generation_idx))
+
+def reflect_all(generation_idx, obs):
+    return list(map(lambda loc: [obs[1] - loc[0] - 1, obs[2] - loc[1] - 1], generation_idx))
+
+def transpose(generation_idx):
+    return list(map(lambda loc: [loc[1], loc[0]], generation_idx))
+
+def augment_orders(generation_idx, obs):
+    return [
+        generation_idx,
+        reflect_rows(generation_idx, obs),
+        reflect_cols(generation_idx, obs),
+        reflect_all(generation_idx, obs),
+        transpose(generation_idx),
+        reflect_rows(transpose(generation_idx), obs),
+        reflect_cols(transpose(generation_idx), obs),
+        reflect_all(transpose(generation_idx), obs)
+    ]
+
+def plot_order(generation_idx, obs, out_path=None):
+    """Plot generation coordinate list. A star on the curve
+    denotes the pixel generated last. obs is a three-tuple of input image dimensions,
+    (input-channels-unused, num_rows, num_cols)"""
+    plt.figure(figsize=(3, 3))
+    plt.hlines(np.arange(-1, obs[1])+0.5, xmin=-0.5, xmax=obs[2]-0.5, alpha=0.5)
+    plt.vlines(np.arange(-1, obs[2])+0.5, ymin=-0.5, ymax=obs[1]-0.5, alpha=0.5)
+    rows, cols = zip(*generation_idx)
+    plt.plot(cols, rows, color="r")
+    plt.scatter([cols[-1]], [rows[-1]], marker="*", s=100, c="k")
+    plt.xticks(np.arange(obs[1]))
+    plt.axis("equal")
+    plt.gca().invert_yaxis()
+    if out_path:
+        plt.savefig(out_path)
+    else:
+        plt.show()
+
+def plot_orders(generation_idx_list, obs, size=5, plot_rows=4, out_path=None):
+    """Plot multiple generation coordinate lists in a single figure. A star on the curve
+    denotes the pixel generated last. obs is a three-tuple of input image dimensions,
+    (input-channels-unused, num_rows, num_cols)"""
+    num = len(generation_idx_list)
+    plot_cols = int(math.ceil(num / 4))
+    fig, axes = plt.subplots(plot_rows, plot_cols, figsize=(size * plot_cols, size * plot_rows))
+    pr, pc = 0, 0
+    for generation_idx in generation_idx_list:
+        axes[pr, pc].hlines(np.arange(-1, obs[1])+0.5, xmin=-0.5, xmax=obs[2]-0.5, alpha=0.5)
+        axes[pr, pc].vlines(np.arange(-1, obs[2])+0.5, ymin=-0.5, ymax=obs[1]-0.5, alpha=0.5)
+        rows, cols = zip(*generation_idx)
+        axes[pr, pc].plot(cols, rows, color="r")
+        axes[pr, pc].scatter([cols[-1]], [rows[-1]], marker="*", s=100, c="k")
+        axes[pr, pc].axis("equal")
+        axes[pr, pc].invert_yaxis()
+        pc = (pc + 1) % plot_cols
+        if pc == 0:
+            pr += 1
+    if out_path:
+        plt.savefig(out_path)
+    else:
+        plt.show()
+
+
+########
+# Masks
+########
 
 def kernel_masks(generation_order_idx: np.ndarray, nrows, ncols, k=3,
                  dilation=1, mask_type='B', set_padding=0) -> np.ndarray:
